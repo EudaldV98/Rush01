d01903788bc3ba94110d53ec6be734c0f27114d8
EudaldV98 <janeudald98@gmail.com>
2022-08-11 14:49:19 +0200

    Doing Rush01

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int	parser(char *str);
+
+int	main(int ac, char **av)
+{
+	parser(av[1]);
+	return 0;
+}
---
+++ parser.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	parser(char *str)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (str[i] >= '1' && str[i] <= '4')
+			{
+				printf("%c", str[i]);
+				j++;
+			}
+			else if (str[i] != ' ')
+				return 1;
+			i++;
+		}
+		printf("\n");
+	}
+	//printf("%s\n", str);
+	return 0;
+}
